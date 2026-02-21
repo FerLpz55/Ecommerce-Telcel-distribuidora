d63deb6f9c760b952e126c64bfca26fcc225d779
AMERICA889 <57241100070@utrng.edu.mx>
2026-02-20 22:39:22 -0600

    configuración de supabase y environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: false,
   apiUrl: '/api',
   // Reemplaza con tus credenciales de Supabase
-  supabaseUrl: 'URL_DE_TU_PROYECTO_SUPABASE',
-  supabaseKey: 'TU_CLAVE_PUBLICA_ANON_DE_SUPABASE'
+  supabaseUrl: 'https://qqrllytqukyamfmxoszi.supabase.co',
+  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFxcmxseXRxdWt5YW1mbXhvc3ppIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzEzOTM5NzUsImV4cCI6MjA4Njk2OTk3NX0.aGH_qJDCiQTprrCmxWIAq36H1bGDuS5dQQPGi3M40iY'
 };
